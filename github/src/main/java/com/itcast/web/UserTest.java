d059423722f04dec36a38960d9bf73453dcb8ee1
lisi <lisi@itcast.com>
2018-12-23 19:56:10 +0800

    提交github222工程到本地仓库

--- github/src/main/java/com/itcast/web/UserTest.java
+++ github/src/main/java/com/itcast/web/UserTest.java
@@ -3,5 +3,11 @@ package com.itcast.web;
 public class UserTest {
     public static void main(String[] args) {
         System.out.println("123...");
+        System.out.println("456...");
+        System.out.println("789...");
+    }
+
+    public void findAll(){
+        System.out.println("ceshi....");
     }
 }
